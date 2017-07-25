78e069d02b27fbce7e0cc2c15d0c6cb837266539
George Madrid <gmadrid@gmail.com>
2017-07-24 23:03:08 -0400

    Initial Commit

---
+++ presq/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  presq
+//
+//  Created by George Madrid on 7/24/17.
+//  Copyright © 2017 George Madrid. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
