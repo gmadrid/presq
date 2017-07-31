afbb6600a56ea65961dd47211bfa21f046895f8d
George Madrid <gmadrid@gmail.com>
2017-07-31 10:32:50 -0400

    swiftformat

--- presq/ImageExtensions.swift
+++ presq/ImageExtensions.swift
@@ -45,8 +45,13 @@ extension CGImage {
     var result = Array(repeating: UInt8(0), count: totalBytes)
 
     let space = CGColorSpaceCreateDeviceGray()
-    guard let context = CGContext(data: &result, width: width, height: height, bitsPerComponent: 8,
-                                  bytesPerRow: width, space: space, bitmapInfo: bitmapInfo.rawValue) else {
+    guard let context = CGContext(data: &result,
+                                  width: width,
+                                  height: height,
+                                  bitsPerComponent: 8,
+                                  bytesPerRow: width,
+                                  space: space,
+                                  bitmapInfo: bitmapInfo.rawValue) else {
       throw Error.GenericError
     }
     context.draw(self, in: size.rect)
