7fee03b277ced9d6b79faaca68a4ba2ca70324e0
George Madrid <gmadrid@gmail.com>
2017-07-31 13:41:27 -0400

    Minor code cleanup

--- presq/CGImageExt.swift
+++ presq/CGImageExt.swift
@@ -58,17 +58,27 @@ extension CGImage {
   }
 
   func ahash() throws -> UInt64 {
-    let grayImage = try toGray()
-    let smallImage = try grayImage.scale(to: CGSize(width: 8, height: 8))
+    let smallImage = try toGray().scale(to: CGSize(width: 8, height: 8))
     let intensityValues = try smallImage.intensities()
 
-    // Compute average in UInt16 to avoid overflow.
-    let avgValue =
-      intensityValues.reduce(UInt16(0)) { $0 + UInt16($1) } / UInt16(intensityValues.count)
+    let avgValue = intensityValues.avg()
 
     let hashBits = intensityValues.map { UInt16($0) < avgValue ? 0 : 1 }
     let hash = hashBits.reduce(UInt64(0)) { ($0 << 1) | UInt64($1) }
 
     return hash
+  }
+}
+
+extension Sequence where Iterator.Element == UInt8 {
+  // Compute sum in UInt16 to avoid overflow.
+  func sum() -> UInt16 {
+    return reduce(UInt16(0)) { $0 + UInt16($1) }
+  }
+}
+
+extension Array where Iterator.Element == UInt8 {
+  func avg() -> UInt16 {
+    return sum() / UInt16(count)
   }
 }
