a59b8dfa583f894cee159f927bfa41aa4c628e45
George Madrid <gmadrid@gmail.com>
2017-08-04 15:08:14 -0400

    LRU now evicts LRU not first added.

--- presq/ImageCache.swift
+++ presq/ImageCache.swift
@@ -14,10 +14,12 @@ class CacheNode<K, V> where K: Hashable {
 
 // An LRU cache
 class ImageCache<K, V> where K: Hashable {
-  var lru: CacheNode<K, V>?
-  var mru: CacheNode<K, V>?
+  typealias NodeType = CacheNode<K, V>
 
-  var values: [K: CacheNode<K, V>] = [:]
+  var lru: NodeType?
+  var mru: NodeType?
+
+  var values: [K: NodeType] = [:]
 
   fileprivate let resolve: (K) throws -> V
 
@@ -29,43 +31,91 @@ class ImageCache<K, V> where K: Hashable {
     self.maxSize = maxSize
   }
 
+  func maybeRemoveLRU() {
+    guard count >= maxSize else { return }
+
+    if let oldLru = lru {
+      values[oldLru.key] = nil
+
+      // If there is an lru, then make it's prev the new lru
+      lru = oldLru.prev
+      lru?.next = nil
+    }
+  }
+
+  func dumpChain() {
+    var keys = [K]()
+    keys.reserveCapacity(count)
+
+    var node_ = mru
+    while let node = node_ {
+      keys.append(node.key)
+      node_ = node.next
+    }
+
+    print("chain from mru: \(keys)")
+
+    keys.removeAll(keepingCapacity: true)
+    node_ = lru
+    while let node = node_ {
+      keys.append(node.key)
+      node_ = node.prev
+    }
+    print("chain from lru: \(keys)")
+    if keys.first as? String == "2" {
+      print("WE ARE HERE")
+    }
+  }
+
+  func addMru(_ node: NodeType) {
+    if let oldMru = mru {
+      oldMru.prev = node
+      node.next = oldMru
+      mru = node
+    } else {
+      // It's the first node
+      mru = node
+      lru = node
+    }
+  }
+
+  func moveToMru(_ node: NodeType) {
+    // Maybe there is nothing to do
+    if node === mru { return }
+
+    // We may have to update the lru value.
+    if node === lru {
+      lru = node.prev
+    }
+
+    // Then, finally, move the node in the list.
+    let oldPrev = node.prev
+    let oldNext = node.next
+    oldPrev?.next = oldNext
+    oldNext?.prev = oldPrev
+
+    // Then fixup the mru value.
+    node.next = mru
+    node.prev = nil
+    mru?.prev = node
+    mru = node
+  }
+
   func find(key: K) throws -> V {
     if let node = values[key] {
       // Found it
+      moveToMru(node)
 
-      // TODO: deal with the lru list
-      print("FOUND IT: \(key)")
       return node.value
     } else {
       let val = try resolve(key)
-      print("INSERTING: \(key)")
 
-      if count >= maxSize {
-        if let oldLru = lru {
-          values.removeValue(forKey: oldLru.key)
+      maybeRemoveLRU()
 
-          let oldPrev = oldLru.prev
-          lru = oldPrev
-          if oldPrev != nil {
-            oldPrev?.next = nil
-          }
-        } else {
-          print("lru never got set somehow. Bummer")
-        }
-      }
-
-      let newNode = CacheNode<K, V>(k: key, v: val)
+      let newNode = NodeType(k: key, v: val)
       values[key] = newNode
 
-      if let oldMru = mru {
-        mru = newNode
-        oldMru.prev = newNode
-        newNode.next = oldMru
-      } else {
-        // It's the first node
-        mru = newNode
-        lru = newNode
-      }
+      addMru(newNode)
 
       return val
     }
--- presqTests/ImageCacheTests.swift
+++ presqTests/ImageCacheTests.swift
@@ -5,29 +5,37 @@ func simpleResolve(key: String) -> Int {
   return Int(key)!
 }
 
+func iter<K, V>(from: CacheNode<K, V>?, next: (CacheNode<K, V>) -> CacheNode<K, V>? = { $0.next }, doing: (CacheNode<K, V>) -> Bool) -> Bool {
+  var node_ = from
+  while let node = node_ {
+    if !doing(node) { return false }
+    node_ = next(node)
+  }
+  return true
+}
+
 class ImageCacheTests: XCTestCase {
 
   func checkConsistent<K, V>(_ cache: ImageCache<K, V>) -> Bool {
     // Check that the mru list and the dict have exactly the same set of keys.
     var keySet = Set(cache.values.keys)
 
-    var node_ = cache.mru
-    while let node = node_ {
+    if !iter(from: cache.mru) { node in
       if !keySet.contains(node.key) {
         print("\(node.key) missing from keyset")
         return false
       }
       keySet.remove(node.key)
-      node_ = node.next
-    }
+      return true
+    } { return false }
+
     if keySet.count > 0 {
       print("Keys still in key set: \(keySet)")
       return false
     }
 
     // Check that node->next->prev == node
-    node_ = cache.mru
-    while let node = node_ {
+    if !iter(from: cache.mru) { node in
       if let next = node.next {
         guard let prev = next.prev else {
           print("Prev for node \(next.key) should not be nil")
@@ -38,16 +46,42 @@ class ImageCacheTests: XCTestCase {
           return false
         }
       }
+      return true
+    } { return false }
 
-      node_ = node.next
+    // LRU and MRU should be reverses.
+    var keysFromMru = [K]()
+    _ = iter(from: cache.mru, next: { $0.next }) {
+      keysFromMru.append($0.key)
+      return true
+    }
+    var keysFromLru = [K]()
+    _ = iter(from: cache.lru, next: { $0.prev }) {
+      keysFromLru.append($0.key)
+      return true
+    }
+    if keysFromMru != keysFromLru.reversed() {
+      print("Chains not equal: \(keysFromMru) != \(keysFromLru)")
+      return false
     }
 
     return true
   }
 
-  func findAndCheck<K, V>(_ cache: ImageCache<K, V>, _ key: K) throws -> V {
+  func checkContains<K, V>(_ cache: ImageCache<K, V>, _ keys: [K]) -> Bool {
+    for key in keys {
+      if cache.values[key] == nil {
+        print("Key, '\(key)', missing from cache")
+        return false
+      }
+    }
+    return true
+  }
+
+  func findAndCheck<K, V>(_ cache: ImageCache<K, V>, _ key: K) throws -> V? {
     let val = try cache.find(key: key)
-    XCTAssert(checkConsistent(cache))
+    if !checkConsistent(cache) { return nil }
+    //    XCTAssert(checkConsistent(cache))
     return val
   }
 
@@ -60,6 +94,8 @@ class ImageCacheTests: XCTestCase {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
+
+  // TODO: add some special cases like node was already mru/lru, or node was second from end.
 
   func testBasic() {
     let cache = ImageCache(maxSize: 100, resolve: simpleResolve)
@@ -92,5 +128,23 @@ class ImageCacheTests: XCTestCase {
     XCTAssertEqual(4, cache.count)
     XCTAssertEqual(5, try findAndCheck(cache, "5"))
     XCTAssertEqual(4, cache.count)
+  }
+
+  func testEvictLru() {
+    let cache = ImageCache(maxSize: 4, resolve: simpleResolve)
+
+    XCTAssertEqual(1, try findAndCheck(cache, "1"))
+    XCTAssertEqual(2, try findAndCheck(cache, "2"))
+    XCTAssertEqual(3, try findAndCheck(cache, "3"))
+    XCTAssertEqual(4, try findAndCheck(cache, "4"))
+    XCTAssert(checkContains(cache, ["1", "2", "3", "4"]))
+    XCTAssertEqual(5, try findAndCheck(cache, "5"))
+    XCTAssertEqual(4, cache.count)
+    XCTAssert(checkContains(cache, ["2", "3", "4", "5"]))
+
+    XCTAssertEqual(2, try findAndCheck(cache, "2"))
+    XCTAssertEqual(6, try findAndCheck(cache, "6"))
+    XCTAssertEqual(4, cache.count)
+    XCTAssert(checkContains(cache, ["2", "4", "5", "6"]))
   }
 }
