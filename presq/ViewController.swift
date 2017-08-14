fdf311d243269f62780f5161246d8ed32a5c424c
George Madrid <gmadrid@gmail.com>
2017-08-14 16:50:21 -0400

    Working on adding CommonCrypto

--- presq/ViewController.swift
+++ presq/ViewController.swift
@@ -54,13 +54,24 @@ class ViewController: NSViewController {
       .debug()
       .observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
       .subscribe(onNext: { [weak self] imageURL in
-        print("HI THERE")
         guard let this = self else { print("FOO"); return }
         guard let cgImage = try? this.imageCache.find(key: imageURL) else { print("BAR"); return }
-        guard let cfData = cgImage.dataProvider?.data else {
-          print("QUUX")
-          return }
-
+        guard let cfData = cgImage.dataProvider?.data else { print("SHOOP"); return }
+        let data = cfData as NSData as Data
+        
+        var digest = Array<UInt8>(repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        let _ = data.withUnsafeBytes { dataPtr in
+          CC_MD5(dataPtr, CC_LONG(data.count), &digest)
+        }
+        print("DIGEST: \(digest)")
+        
+//        let context = UnsafeMutablePointer<CC_MD5_CTX>.allocate(capacity: 1)
+//        CC_MD5(<#T##data: UnsafeRawPointer!##UnsafeRawPointer!#>, <#T##len: CC_LONG##CC_LONG#>, <#T##md: UnsafeMutablePointer<UInt8>!##UnsafeMutablePointer<UInt8>!#>)
+//        CC_MD5_Init(context)
+//        CC_MD5_Update(context, cfData, CC_LONG(cfData.count))
+//        CC_MD5_Final(&digest, context)
+//        context.deallocate(capacity: 1)
+//
         print("BAMMBM")
 //        let hash = (cfData as NSData as Data).md5()
         print("GEORGE")
